140933b5445ef3c7d372f10e577af874438275ac
Exec <86654007+Exec7@users.noreply.github.com>
2021-12-08 11:36:23 +0300

    Create legitmode.lua

---
+++ legitmode.lua
@@ -0,0 +1,6 @@
+local a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p=exechack_api.cfg_SetValue,
+player.GetAll,"AIM_NOSPREAD",false,ipairs,"AIM_SILENT","Think",
+hook.Add,true,LocalPlayer(),4,0,"AIM_SMOOTHING",FindMetaTable("Player")
+q=n.GetObserverTarget;h(g,c,function()o=d;for r,s in e(b())do if
+q(s)==j then o=d end end;if p~=o then p=o;if o then a(c,d)a(f,d)
+a(m,k)else a(c,i)a(f,i)a(m,l)end end end)
