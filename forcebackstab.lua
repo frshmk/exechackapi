4697646fbc5f1b99ed1e99efc5fbf6d063b06666
Exec <86654007+Exec7@users.noreply.github.com>
2021-12-09 11:05:44 +0300

    Create forcebackstab.lua

---
+++ forcebackstab.lua
@@ -0,0 +1,12 @@
+ 	function Backstab( ucmd )
+		viewang = ucmd:GetViewAngles()
+            if(input.WasKeyPressed(KEY_E)) then
+        end
+        if(backstabdelay == true) then
+        	ucmd:SetViewAngles(viewang + Angle(45, 90 , 0))
+        	RunConsoleCommand("+attack2")
+            timer.Simple(0.07, function() RunConsoleCommand("-attack2") end)
+            timer.Simple(0.05, function() backstabdelay = false end)
+        end
+    end
+hook.Add("CreateMove", "Backstab", Backstab)
